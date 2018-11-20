e3a050ea9c365a436b67363d47399e6304cfc828
John (Jack) Brown <jack@brownjohnf.com>
2018-11-20 11:22:04 -0800

    Working basic install

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
 # WARNING: this script will destroy data on the selected disk.
-# This script can be run by executing the following:
-#   curl -sL https://git.io/vAoV8 | bash
-set -uo pipefail
+
+set -euo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
-REPO_URL="https://s3.eu-west-2.amazonaws.com/mdaffin-arch/repo/x86_64"
-MIRRORLIST_URL="https://www.archlinux.org/mirrorlist/?country=GB&protocol=https&use_mirror_status=on"
+# Ensure we've got the system booted in EFI
+ls /sys/firmware/efi/efivars > /dev/null
+
+#REPO_URL="https://s3.eu-west-2.amazonaws.com/mdaffin-arch/repo/x86_64"
+MIRRORLIST_URL="https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on"
 
 pacman -Sy --noconfirm pacman-contrib
 
@@ -24,10 +27,13 @@ user=$(dialog --stdout --inputbox "Enter admin username" 0 0) || exit 1
 clear
 : ${user:?"user cannot be empty"}
 
-password=$(dialog --stdout --passwordbox "Enter admin password" 0 0) || exit 1
+password=$(dialog --stdout --passwordbox "Enter admin password (will not print)" 0 0) || exit 1
 clear
 : ${password:?"password cannot be empty"}
-password2=$(dialog --stdout --passwordbox "Enter admin password again" 0 0) || exit 1
+password2=$( \
+  dialog --stdout --passwordbox \
+  "Enter admin password again (will not print)" 0 0 \
+) || exit 1
 clear
 [[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
 
@@ -42,50 +48,58 @@ exec 2> >(tee "stderr.log")
 timedatectl set-ntp true
 
 ### Setup the disk and partitions ###
-swap_size=$(free --mebi | awk '/Mem:/ {print $2}')
-swap_end=$(( $swap_size + 129 + 1 ))MiB
-
-parted --script "${device}" -- mklabel gpt \
+parted --script "${device}" -- \
+  mklabel gpt \
   mkpart ESP fat32 1Mib 129MiB \
   set 1 boot on \
-  mkpart primary linux-swap 129MiB ${swap_end} \
-  mkpart primary ext4 ${swap_end} 100%
+  mkpart primary ext4 129MiB 100%
 
 # Simple globbing was not enough as on one device I needed to match /dev/mmcblk0p1
 # but not /dev/mmcblk0boot1 while being able to match /dev/sda1 on other devices.
 part_boot="$(ls ${device}* | grep -E "^${device}p?1$")"
-part_swap="$(ls ${device}* | grep -E "^${device}p?2$")"
-part_root="$(ls ${device}* | grep -E "^${device}p?3$")"
+part_root="$(ls ${device}* | grep -E "^${device}p?2$")"
 
 wipefs "${part_boot}"
-wipefs "${part_swap}"
 wipefs "${part_root}"
 
 mkfs.vfat -F32 "${part_boot}"
-mkswap "${part_swap}"
 mkfs.f2fs -f "${part_root}"
 
-swapon "${part_swap}"
 mount "${part_root}" /mnt
 mkdir /mnt/boot
 mount "${part_boot}" /mnt/boot
 
 ### Install and configure the basic system ###
-cat >>/etc/pacman.conf <<EOF
-[mdaffin]
-SigLevel = Optional TrustAll
-Server = $REPO_URL
-EOF
 
-pacstrap /mnt mdaffin-desktop
+#cat >>/etc/pacman.conf <<EOF
+#[mdaffin]
+#SigLevel = Optional TrustAll
+#Server = $REPO_URL
+#EOF
+
+#usage: pacstrap [options] root [packages...]
+#
+#  Options:
+#    -C config      Use an alternate config file for pacman
+#    -c             Use the package cache on the host, rather than the target
+#    -G             Avoid copying the host's pacman keyring to the target
+#    -i             Prompt for package confirmation when needed
+#    -M             Avoid copying the host's mirrorlist to the target
+#
+#    -h             Print this help message
+#
+#pacstrap installs packages to the specified new root directory. If no packages
+#are given, pacstrap defaults to the "base" group.
+
+pacstrap /mnt base sudo zsh # base-devel networkmanager
 genfstab -t PARTUUID /mnt >> /mnt/etc/fstab
 echo "${hostname}" > /mnt/etc/hostname
 
-cat >>/mnt/etc/pacman.conf <<EOF
-[mdaffin]
-SigLevel = Optional TrustAll
-Server = $REPO_URL
-EOF
+#cat >>/mnt/etc/pacman.conf <<EOF
+#[mdaffin]
+#SigLevel = Optional TrustAll
+#Server = $REPO_URL
+#EOF
 
 arch-chroot /mnt bootctl install
 
@@ -100,10 +114,33 @@ initrd   /initramfs-linux.img
 options  root=PARTUUID=$(blkid -s PARTUUID -o value "$part_root") rw
 EOF
 
-echo "LANG=en_GB.UTF-8" > /mnt/etc/locale.conf
+
+# Set the timezone
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
+
+# Sync time
+arch-chroot /mnt hwclock --systohc
+
+# Set up tho locale
+echo "en_US.UTF-8 UTF-8" > /mnt/etc/locale.gen
+arch-chroot /mnt locale-gen
+echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
+echo "KEYMAP=dvorak" > /mnt/etc/vconsole.conf
+
+hostname_short=$(echo $hostname | cut -d '.' -f 1)
+cat <<EOF > /mnt/etc/hosts
+127.0.0.1 localhost
+::1       localhost
+::1       $hostname $hostname_short
+127.0.1.1	$hostname $hostname_short
+EOF
 
 arch-chroot /mnt useradd -mU -s /usr/bin/zsh -G wheel,uucp,video,audio,storage,games,input "$user"
-arch-chroot /mnt chsh -s /usr/bin/zsh
+touch /mnt/home/$user/.zshrc
+arch-chroot /mnt chown $user: /mnt/home/$user/.zshrc
+
+# Change root user's shell to zsh
+#arch-chroot /mnt chsh -s /usr/bin/zsh
 
 echo "$user:$password" | chpasswd --root /mnt
 echo "root:$password" | chpasswd --root /mnt

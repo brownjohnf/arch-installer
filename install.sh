d5390bb77288fa16f83f115ffdc6cc3d439fd336
Jack Brown <jack@brownjohnf.com>
2020-02-29 22:58:23 -0800

    yank lvm

--- install.sh
+++ install.sh
@@ -6,18 +6,10 @@ set -xeuo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
 CLEAN=${CLEAN:-''}
-ZFS=${ZFS:-''}
 
 function clean() {
   umount /mnt/boot || true
-  umount /mnt/var || true
-  umount /mnt/home || true
-  umount /mnt/data || true
-  umount /mnt || true
-
-  vgchange -a n lvmroot || true
-  pvremove -ff /dev/mapper/cryptroot || true
-
+  zfs umount -a || true
   zpool destroy zroot || true
 
   cryptsetup close /dev/mapper/cryptroot || true
@@ -43,39 +35,6 @@ if [ -z "$CLEAN" ]; then
       rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 fi
 
-### Get infomation from user ###
-devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
-device=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1
-clear
-
-luks_password=$(dialog --stdout --inputbox "Enter LUKS password (will print)" 0 0) || exit 1
-clear
-: ${luks_password:?"luks_password cannot be empty"}
-
-hostname=$(dialog --stdout --inputbox "Enter hostname" 0 0) || exit 1
-clear
-: ${hostname:?"hostname cannot be empty"}
-
-wifi=false
-if dialog --yesno "Configure wifi for target system?" 0 0; then
-  wifi=true
-fi
-clear
-
-if [ "$wifi" == "true" ]; then
-  wifi_ssid=$(dialog --stdout --inputbox "Enter wifi SSID" 0 0) || exit 1
-  clear
-  : ${wifi_ssid:?"Wifi SSID cannot be empty"}
-
-  wifi_password=$(dialog --stdout --inputbox "Enter wifi password (will print)" 0 0) || exit 1
-  clear
-  : ${wifi_password:?"Wifi password cannot be empty"}
-fi
-
-user=$(dialog --stdout --inputbox "Enter admin username" 0 0) || exit 1
-clear
-: ${user:?"user cannot be empty"}
-
 ### Set up logging ###
 exec 1> >(tee "stdout.log")
 exec 2> >(tee "stderr.log")
@@ -85,6 +44,10 @@ timedatectl set-ntp true
 ### Setup the disk and partitions ###
 ### Make sure we provide enough room on boot for multiple kernels.
 ### We also create a 2G partition for encrypted secrets storage.
+devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
+device=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1
+clear
+
 parted --script "${device}" -- \
   mklabel gpt \
   mkpart ESP fat32 1Mib 2GiB \
@@ -105,93 +68,50 @@ dd if=/dev/urandom of="$part_root" bs=512 count=20480
 # Set up the EFI partition
 mkfs.vfat -F32 "${part_boot}"
 
+# Setup the full disk encryption
+# TODO: replace this with native ZFS encryption
+luks_password=$(dialog --stdout --inputbox "Enter LUKS password (will print)" 0 0) || exit 1
+clear
+: ${luks_password:?"luks_password cannot be empty"}
+
 cryptsetup -v luksFormat --type luks2 "$part_root" <<< "${luks_password}"
 cryptsetup open "$part_root" cryptroot <<< "${luks_password}"
 
-# If we're doing ZFS, we set up ZFS partitioning
-if [ -n "$ZFS" ]; then
-  # Ensure the module's loaded
-  modprobe zfs
+# Set up ZFS partitioning
+# Ensure the module's loaded
+modprobe zfs
 
-  # Create a pool made up of the drive
-  zpool create -f zroot /dev/disk/by-id/dm-name-cryptroot
+# Create a pool made up of the drive
+zpool create -f zroot /dev/disk/by-id/dm-name-cryptroot
 
-  # Create the root, with default attributes
-  zfs create -o atime=off -o compression=on -o mountpoint=none zroot/ROOT
-  # Create /
-  zfs create -o mountpoint=/ zroot/ROOT/default || true
+# Create the root, with default attributes
+zfs create -o atime=off -o compression=on -o mountpoint=none zroot/ROOT
+# Create /
+zfs create -o mountpoint=/ zroot/ROOT/default || true
 
-  # Create datasets for all the other partitions we want to isolate, setting
-  # their mountpoint
-  for path in /home /var /var/log /var/log/journal /etc /data /data /docker; do
-    zfs create -o mountpoint=$path zroot/ROOT$path || true
-  done
-  # Unmount everything for now
-  zfs unmount -a
+# Create datasets for all the other partitions we want to isolate, setting
+# their mountpoint
+for path in /home /var /var/log /var/log/journal /etc /data /data /docker; do
+  zfs create -o mountpoint=$path zroot/ROOT$path || true
+done
+# Unmount everything for now
+zfs unmount -a
 
-  # Set up posix ACL for the journal
-  zfs set acltype=posixacl zroot/ROOT/var/log/journal
-  zfs set xattr=sa zroot/ROOT/var/log/journal
+# Set up posix ACL for the journal
+zfs set acltype=posixacl zroot/ROOT/var/log/journal
+zfs set xattr=sa zroot/ROOT/var/log/journal
 
-  echo "zroot/ROOT/default / zfs defaults,noatime 0 0" >> /etc/fstab
-  zpool set bootfs=zroot/ROOT/default zroot
-  zpool export zroot
+echo "zroot/ROOT/default / zfs defaults,noatime 0 0" >> /etc/fstab
+zpool set bootfs=zroot/ROOT/default zroot
+zpool export zroot
 
-  # import the pool by id, to ensure get consistent mounting
-  zpool import -d /dev/disk/by-id -R /mnt zroot
-  zpool set cachefile=/etc/zfs/zpool.cache zroot
-  mkdir -p /mnt/etc/zfs
-  cp /etc/zfs/zpool.cache /mnt/etc/zfs/zpool.cache
-  mkdir -p /mnt/boot
-  mount "$part_boot" /mnt/boot
-else
-  # If we're doing LVM, then we'll set up LVM partitions
-  pvcreate /dev/mapper/cryptroot
-  vgcreate lvmroot /dev/mapper/cryptroot
-  # If the root partition is less than 10G, make a 3G root partition
-  if [ "$(lsblk -o SIZE --noheadings --nodeps -b "$part_root")" -lt 10000000000 ]; then
-    lvcreate -L 3GB lvmroot -n root
-  else
-    lvcreate -l 15%VG lvmroot -n root
-  fi
-  lvcreate -l 10%VG lvmroot -n var
-  lvcreate -l 20%VG lvmroot -n home
-  lvcreate -l 100%FREE lvmroot -n data
-
-  mkfs.ext4 /dev/lvmroot/root
-  mkfs.ext4 /dev/lvmroot/var
-  mkfs.ext4 /dev/lvmroot/home
-  mkfs.ext4 /dev/lvmroot/data
-
-  function mount_all () {
-    sleep 1
-    mount /dev/lvmroot/root /mnt
-
-    mkdir -p /mnt/{boot,var,home,data}
-
-    mount "${part_boot}" /mnt/boot
-    mount /dev/lvmroot/var /mnt/var
-    mount /dev/lvmroot/home /mnt/home
-    mount /dev/lvmroot/data /mnt/data
-  }
-  mount_all
-
-  # Unmount, lock, unlock and remount the partition to ensure it works
-  umount /mnt/boot
-  umount /mnt/var
-  umount /mnt/home
-  umount /mnt/data
-  umount /mnt
-
-  # Deactivate the volume group. If you don't do this, you can't close the
-  # cryptroot
-  vgchange -a n lvmroot
-
-  cryptsetup close cryptroot
-  cryptsetup open "$part_root" cryptroot <<< "${luks_password}"
-
-  mount_all
-fi
+# import the pool by id, to ensure get consistent mounting
+zpool import -d /dev/disk/by-id -R /mnt zroot
+zpool set cachefile=/etc/zfs/zpool.cache zroot
+mkdir -p /mnt/etc/zfs
+cp /etc/zfs/zpool.cache /mnt/etc/zfs/zpool.cache
+mkdir -p /mnt/boot
+mount "$part_boot" /mnt/boot
 
 ### Install and configure the basic system ###
 
@@ -212,8 +132,9 @@ pacstrap /mnt \
   base \
   linux \
   linux-firmware \
-  linux-lts-headers \
+  linux-headers \
   linux-lts \
+  linux-lts-headers \
   neovim \
   networkmanager \
   openssh \
@@ -224,37 +145,37 @@ pacstrap /mnt \
 genfstab -t PARTUUID /mnt >> /mnt/etc/fstab
 
 # Set up the hostname and /etc/hosts
-echo "$hostname" > /mnt/etc/hostname
-hostname_short="$(echo "$hostname" | cut -d '.' -f 1)"
-cat <<EOF > /mnt/etc/hosts
-127.0.0.1 localhost
-::1       localhost
-::1       $hostname $hostname_short
-127.0.1.1	$hostname $hostname_short
-EOF
+function set_hostname() {
+  hostname=$(dialog --stdout --inputbox "Enter hostname" 0 0) || exit 1
+  clear
+  : ${hostname:?"hostname cannot be empty"}
 
-# Set up the hooks correctly for allowing us to unlock the encrypted partitions
-grep -E '^HOOKS' "/mnt/etc/mkinitcpio.conf" > original_hooks.txt
-if [ -n "$ZFS" ]; then
-  cat <<EOF >>/mnt/etc/pacman.conf
+  echo "$hostname" > /mnt/etc/hostname
+
+  hostname_short="$(echo "$hostname" | cut -d '.' -f 1)"
+  cat <<EOF > /mnt/etc/hosts
+  127.0.0.1 localhost
+  ::1       localhost
+  ::1       $hostname $hostname_short
+  127.0.1.1	$hostname $hostname_short
+  EOF
+}
+set_hostname
+
+# Install zfs modules
+cat <<EOF >>/mnt/etc/pacman.conf
 [archzfs]
 Server = https://archzfs.com/\$repo/x86_64
 EOF
 
-  arch-chroot /mnt pacman-key --recv-keys F75D9D76
-  arch-chroot /mnt pacman-key --lsign-key F75D9D76
-  arch-chroot /mnt pacman -Sy --needed --noconfirm zfs-linux-lts
+arch-chroot /mnt pacman-key --recv-keys F75D9D76
+arch-chroot /mnt pacman-key --lsign-key F75D9D76
+arch-chroot /mnt pacman -Sy --needed --noconfirm zfs-linux zfs-linux-lts
 
-  sed -i \
-    's/^HOOKS.*/HOOKS=(base udev keyboard keymap autodetect modconf block encrypt zfs filesystems fsck)/' \
-    /mnt/etc/mkinitcpio.conf
-else
-  # Install lvm2 so the hooks will work
-  arch-chroot /mnt pacman -Sy --needed --noconfirm lvm2
-  sed -i \
-    's/^HOOKS.*/HOOKS=(base udev keyboard keymap autodetect modconf block encrypt lvm2 filesystems fsck)/' \
-    /mnt/etc/mkinitcpio.conf
-fi
+# Set up the hooks correctly for allowing us to unlock the encrypted partitions
+sed -i \
+  's/^HOOKS.*/HOOKS=(base udev keyboard keymap autodetect modconf block encrypt zfs filesystems)/' \
+  /mnt/etc/mkinitcpio.conf
 
 # Install the bootloader
 arch-chroot /mnt bootctl install
@@ -265,8 +186,22 @@ arch-chroot /mnt systemctl enable NetworkManager.service
 arch-chroot /mnt systemctl enable systemd-timesyncd.service
 
 # set up wifi
+wifi=false
+if dialog --yesno "Configure wifi for target system?" 0 0; then
+  wifi=true
+fi
+clear
+
 if [ "$wifi" == "true" ]; then
-cat <<EOF > "/mnt/etc/NetworkManager/system-connections/$wifi_ssid.nmconnection"
+  wifi_ssid=$(dialog --stdout --inputbox "Enter wifi SSID" 0 0) || exit 1
+  clear
+  : ${wifi_ssid:?"Wifi SSID cannot be empty"}
+
+  wifi_password=$(dialog --stdout --inputbox "Enter wifi password (will print)" 0 0) || exit 1
+  clear
+  : ${wifi_password:?"Wifi password cannot be empty"}
+
+  cat <<EOF > "/mnt/etc/NetworkManager/system-connections/$wifi_ssid.nmconnection"
 [connection]
 id=$wifi_ssid
 type=wifi
@@ -296,23 +231,17 @@ default arch
 timeout 3
 EOF
 
-boot_options="cryptdevice=PARTUUID=$(blkid -s PARTUUID -o value "$part_root"):cryptroot"
+boot_options="cryptdevice=PARTUUID=$(blkid -s PARTUUID -o value "$part_root"):cryptroot zfs=zroot rw"
 
-if [ -n "$ZFS" ]; then
-  boot_options="$boot_options zfs=zroot rw"
-else
-  boot_options="$boot_options root=/dev/lvmroot/root rw"
-fi
+# Truncate the vconsole file, since we'll append to it below, and don't want to
+# duplicate contents on subsequent runs of the script.
+truncate --size 0 /mnt/etc/vconsole.conf
 
 product_name=$(dmidecode \
   | grep -A 3 'System Information' \
   | grep 'Product Name' \
   | cut -d : -f 2 \
   | tr -d '[:blank:]')
-
-# Truncate the vconsole file, since we'll append to it below, and don't want to
-# duplicate contents on subsequent runs of the script.
-truncate --size 0 /mnt/etc/vconsole.conf
 
 # TODO: Use regex here instead of all the grepping and cutting.
 if [ "${product_name}" == "XPS159560" ]; then
@@ -356,12 +285,16 @@ echo "KEYMAP=dvorak" >> /mnt/etc/vconsole.conf
 arch-chroot /mnt mkinitcpio -p linux
 arch-chroot /mnt mkinitcpio -p linux-lts
 
+# figure out which user we want to use
+user=$(dialog --stdout --inputbox "Enter admin username" 0 0) || exit 1
+clear
+: ${user:?"user cannot be empty"}
+
 arch-chroot /mnt useradd -mU \
   --uid 1185 \
   -G wheel,uucp,video,audio,storage,games,input \
   "$user"
 
-if [ -n "$ZFS" ]; then
 cat <<EOF > "/mnt/home/$user/first-boot.sh"
 #!/bin/bash
 
@@ -407,13 +340,12 @@ EOF
 
 # Make sure everything in the user's home directory is owned by them
 arch-chroot /mnt chown -R "$user:" "/home/$user"
-fi
 
 # Set up the wheel group to have sudo access
 echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
 
 # Set passwords for the default admin user and root
-# TODO: Add option to generate random password for root
+# TODO: Set root password to random string
 for u in $user root; do
   echo -n "$u password: "; arch-chroot /mnt passwd "$u"
 done
@@ -421,11 +353,10 @@ done
 # Copy this script into the new installation for reference
 cp "$0" /mnt/home/$user/$(basename "$0")
 
-if [ -n "$ZFS" ]; then
-  umount /mnt/boot
-  zfs umount -a
-  zpool export zroot
-fi
+# Unmount everything to get ready for reboot
+umount /mnt/boot
+zfs umount -a
+zpool export zroot
 
 cat <<EOF
 
@@ -433,7 +364,6 @@ cat <<EOF
 
 Installation complete!
 
-You may now unmount everything and reboot, or enter the installed environment
-via arch-chroot /mnt.
+You may now reboot, or enter the installed environment
+via `arch-chroot /mnt`.
 EOF
-

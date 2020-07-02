f3fa8f678158aff46d775676f6101944ea849d91
Jack Brown <jack@brownjohnf.com>
2020-07-01 17:32:02 -0700

    continue working on functionality

--- install.sh
+++ install.sh
@@ -47,16 +47,21 @@ devicelist=$(lsblk --nodeps --paths --list --noheadings --sort size -o name,size
 device=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1
 clear
 
+# Make 3 partitions:
+# * 2G boot
+# * 1G secrets/encryption/whatev partition
+# * remaining root partition, for ZFS
 parted --script "${device}" -- \
   mklabel gpt \
   mkpart ESP fat32 1Mib 2GiB \
   set 1 boot on \
-  mkpart primary ext4 2GiB 100%
+  mkpart primary ext4 2GiB 3GiB \
+  mkpart primary ext4 3GiB 100%
 
 # Simple globbing was not enough as on one device I needed to match /dev/mmcblk0p1
 # but not /dev/mmcblk0boot1 while being able to match /dev/sda1 on other devices.
 part_boot="$(ls "${device}"* | grep -E "^${device}p?1$")"
-part_root="$(ls "${device}"* | grep -E "^${device}p?2$")"
+part_root="$(ls "${device}"* | grep -E "^${device}p?3$")"
 
 # Wipe any old fs stuff from the new partitions
 wipefs "${part_boot}"
@@ -141,11 +146,6 @@ pacstrap /mnt \
   openssh \
   sudo \
   tmux
-
-# Make the mountpoint for /mnt/pw and set permissions. Needs to happen after the
-# pacstrap so that /mnt will exist in the chroot.
-mkdir /mnt/mnt/pw
-zfs mount zroot/pw
 
 # Generate an fstab and put it in the chroot environment. We only want the boot
 # partition in it; the rest gets handled by ZFS
@@ -309,9 +309,6 @@ function add_user() {
 
   # Set the password
   echo -n "$user password: "; arch-chroot /mnt passwd "$user"
-
-  # Set the /mnt/pw directory to be owned by the user
-  arch-chroot /mnt chown -R "$user:" /mnt/pw
 }
 add_user
 
@@ -381,8 +378,6 @@ cp "$0" /mnt/home/$user/$(basename "$0")
 
 # Unmount everything to get ready for reboot
 umount /mnt/boot
-# Have to manually unmount pw because it's mount setting is noauto.
-zfs umount zroot/pw
 zfs umount -a
 zpool export zroot
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, Context, Result};
+use anyhow::{anyhow, format_err, Context, Result};
 use dialoguer::{theme::ColorfulTheme, Confirm, Input, Select};
 use log::{debug, error, info, warn};
 use simplelog::{CombinedLogger, Config, LevelFilter, TermLogger, TerminalMode, WriteLogger};
@@ -46,10 +46,6 @@ struct Opt {
     // Path to the device to install the system on. Will prompt if not passed.
     #[structopt(short, long)]
     device: Option<PathBuf>,
-
-    // Execute as dry-run; parse the options but don't touch the system.
-    #[structopt(short = "n", long)]
-    dry_run: bool,
 }
 
 // Set the mirrorlist for install.
@@ -57,28 +53,23 @@ struct Opt {
 const MIRRORLIST_URL: &str =
     "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on";
 
-fn main() {
+fn main() -> Result<()> {
+    // Set up the logger to log to terminal and disk, for debugging later.
     CombinedLogger::init(vec![
         WriteLogger::new(
             LevelFilter::Debug,
             Config::default(),
-            fs::File::create("/tmp/arch-installer.log").unwrap(),
+            fs::File::create("arch-installer.log")?,
         ),
         TermLogger::new(LevelFilter::Debug, Config::default(), TerminalMode::Mixed),
     ])
     .unwrap();
 
+    // Parse the arguments.
     let opt = Opt::from_args();
     debug!("{:?}", opt);
 
-    if let Err(e) = run(opt) {
-        error!("{}", e);
-        process::exit(1);
-    }
-}
-
-fn run(opt: Opt) -> Result<()> {
-    // Set up all our inputs
+    // Configure the arguments, setting defaults and such.
     let filesystem = if let Some(f) = opt.filesystem {
         f
     } else {
@@ -92,27 +83,33 @@ fn run(opt: Opt) -> Result<()> {
     };
     let filesystem = filesystem::from_str(filesystem)?;
     debug!("filesystem: {}", filesystem);
+
     let clean = opt.clean;
     debug!("clean up: {}", clean);
+
     let hostname = if let Some(f) = opt.hostname {
         f
     } else {
         input("What should the hostname be?")?
     };
     debug!("hostname: {}", hostname);
+
     let wifi = if let Some(w) = opt.wifi {
         w
     } else {
         confirm("Would you like to configured WiFi?")?
     };
     debug!("configure wifi: {}", wifi);
+
+    // Set the user to set up as the default non-privileged user.
     let user = if let Some(u) = opt.username {
         u
     } else {
         input("Default username?")?
     };
     debug!("default user: {}", user);
-    // Select the device
+
+    // Select the device we're going to install the OS onto.
     let device = if let Some(d) = opt.device {
         Device::from_path(d)
     } else {
@@ -125,24 +122,10 @@ fn run(opt: Opt) -> Result<()> {
         )?;
 
         // Warning: this will panic if i is out of bounds.
+        // TODO: Come up with a better way to get this data.
         dev.remove(i)
     };
     debug!("device: {:?}", device);
-
-    if opt.dry_run {
-        warn!("running in dry-run mode; exiting now");
-        return Ok(());
-    }
-
-    if confirm("Installation will be destructive; continue?")? {
-        error!("no confirmation received; aborting");
-        process::exit(1);
-    }
-
-    // Clean the system if was requested.
-    if clean {
-        filesystem.cleanup()?;
-    }
 
     // Ensure the system is booted in EFI.
     assert_efi()?;
@@ -152,23 +135,10 @@ fn run(opt: Opt) -> Result<()> {
         return Err(anyhow!("zfs module is not loaded"));
     }
 
-    // Rank the mirrors, but only if we're not cleaning up, indicating this is a
-    // fresh run. This is because ranking takes a while.
-    // TODO: Do all this straight in rust using reqwest or something.
-    if !clean {
-        if !exec(&[
-            "bash",
-            "-c",
-            &format!(
-                "curl -s {} | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist",
-                MIRRORLIST_URL
-            ),
-        ])?
-        .status
-        .success()
-        {
-            return Err(anyhow!("error setting up mirrors: {:?}"));
-        }
+    // Make sure the user understands this is going to be destructive.
+    if !confirm("Installation will be destructive; continue?")? {
+        error!("no confirmation received; aborting");
+        process::exit(1);
     }
 
     // Set the datetime from NTP.
@@ -176,29 +146,79 @@ fn run(opt: Opt) -> Result<()> {
         return Err(anyhow!("error setting system time from ntp"));
     }
 
+    // Rank the mirrors, but only if we're not cleaning up, indicating this is a
+    // fresh run. This is because ranking takes a while. This will just write to
+    // the ISO.
+    if !clean {
+        rankmirrors()?;
+    }
+
+    // Clean up the system if was requested. This is mostly needed if the
+    // installer has been run before unsuccessfully.
+    if clean {
+        filesystem.cleanup()?;
+    }
+
     // Partition the disk. Make a large /boot partition so that we have room for
     // multiple kernels, etc.
+    partition(&device)?;
+
+    // Get our partitions.
+    let parts = device.partitions()?;
+    let part_boot = &parts[0];
+    let part_root = &parts[2];
+
+    wipe(part_boot)?;
+    wipe(part_root)?;
+
+    Ok(())
+}
+
+fn wipe(partition: &Device) -> Result<()> {
+    if !exec(&["wipefs", &partition.dev()])?.status.success() {
+        return Err(format_err!("error wiping partition {:?}", partition));
+    }
+
+    Ok(())
+}
+
+fn partition(device: &Device) -> Result<()> {
     if !exec(&[
         "parted",
         "--script",
         &device.dev(),
         "--",
-        "
-  mklabel gpt \
-  mkpart ESP fat32 1Mib 2GiB \
-  set 1 boot on \
-  mkpart primary ext4 2GiB 100%",
+        // Make the partition table.
+        "mklabel",
+        "gpt",
+        // Make the boot partition for EFI.
+        "mkpart",
+        "ESP",
+        "fat32",
+        "1Mib",
+        "2GiB",
+        "set",
+        "1",
+        "boot",
+        "on",
+        // Make a persistent small partition for things like encrypted storage.
+        "mkpart",
+        "primary",
+        "ext4",
+        "2GiB",
+        "3GiB",
+        // Make the ZFS root partition.
+        "mkpart",
+        "primary",
+        "ext4",
+        "3GiB",
+        "100%",
     ])?
     .status
     .success()
     {
-        return Err(anyhow!("error partitioning disk"));
+        return Err(format_err!("error partitioning disk"));
     }
-
-    // Get our partitions.
-    let parts = device.partitions()?;
-    let part_boot = &parts[0];
-    let part_root = &parts[1];
 
     Ok(())
 }
@@ -216,7 +236,9 @@ fn assert_efi() -> Result<()> {
         return Ok(());
     }
 
-    Err(anyhow!("system does not appear to be booted in EFI mode"))
+    Err(format_err!(
+        "system does not appear to be booted in EFI mode"
+    ))
 }
 
 fn exec(cmd: &[&str]) -> Result<process::Output> {
@@ -224,7 +246,7 @@ fn exec(cmd: &[&str]) -> Result<process::Output> {
 
     let (cmd, args) = match cmd {
         [cmd, args @ ..] => (cmd, args),
-        _ => return Err(anyhow!("missing command".to_string())),
+        _ => return Err(format_err!("missing command".to_string())),
     };
 
     Ok(process::Command::new(cmd)
@@ -240,4 +262,25 @@ fn select(title: &str, items: &[String]) -> Result<usize> {
         .default(0)
         .items(items)
         .interact()?)
+}
+
+/// Calculate the most efficient mirrors to use with pacman. This can make a
+/// huge difference on install time, as well as normal system updates.
+// TODO: Do all this straight in rust using reqwest or something.
+fn rankmirrors() -> Result<()> {
+    if !exec(&[
+            "bash",
+            "-c",
+            &format!(
+                "curl -s {} | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist",
+                MIRRORLIST_URL
+            ),
+        ])?
+        .status
+        .success()
+        {
+            return Err(format_err!("error setting up mirrors: {:?}"));
+        }
+
+    Ok(())
 }

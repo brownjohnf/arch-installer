206657b7af8e00d234c63a8c65fbd19222463267
Jack Brown <jack@remind101.com>
2020-08-21 17:43:44 -0700

    WIP on refactoring; zfs should be done but needs testing

--- install.sh
+++ install.sh
@@ -5,13 +5,23 @@
 set -xeuo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
-CLEAN=${CLEAN:-''}
+### Set up logging ###
+exec 1> >(tee "stdout.log")
+exec 2> >(tee "stderr.log")
 
-function clean() {
-  umount /mnt/boot || true
-  zfs umount -a || true
-  zpool destroy zroot || true
-}
+if "${FS}" == "zfs"; then
+  source ./zfs.sh
+elif "${FS}" == "ext4"; then
+  source ./ext4.sh
+else
+  echo "You must specify FS=zfs or FS=ext4!"
+  exit 1
+fi
+
+CLEAN=${CLEAN:-""}
+
+# Set this to 'dvorak' for a dvorak keymap on a laptop.
+KEYMAP=${KEYMAP:-""}
 
 # If CLEAN is set, clean up any broken state we may have lying around
 if [ -n "$CLEAN" ]; then
@@ -21,8 +31,9 @@ fi
 # Ensure we've got the system booted in EFI
 ls /sys/firmware/efi/efivars > /dev/null
 
-# Ensure we have the zfs module
-modprobe zfs
+# Check to ensure we've got whatever dependencies we need for whichever mode
+# we're running in (ZFS, ext4, etc.)
+checkdeps
 
 MIRRORLIST_URL="https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on"
 
@@ -33,10 +44,6 @@ if [ -z "$CLEAN" ]; then
       sed -e 's/^#Server/Server/' -e '/^#/d' | \
       rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 fi
-
-### Set up logging ###
-exec 1> >(tee "stdout.log")
-exec 2> >(tee "stderr.log")
 
 # Set the time from NTP
 timedatectl set-ntp true
@@ -73,46 +80,8 @@ dd if=/dev/urandom of="$part_root" bs=512 count=20480
 # Set up the EFI partition
 mkfs.vfat -F32 "${part_boot}"
 
-# Set up ZFS partitioning
-# Create a pool made up of the drive
-zpool create -f zroot -m none "$part_root"
-
-# Create the root (/), with default attributes (|| true because it will fail to
-# mount)
-# TODO: Figure out a better way to recover from the mount failure. Catching any
-# error is not a good idea.
-zfs create \
-  -o atime=off \
-  -o compression=on \
-  -o mountpoint=/ \
-  -o encryption=on \
-  -o keyformat=passphrase \
-  zroot/ROOT || true
-
-# Create datasets for all the other partitions we want to isolate, setting
-# their mountpoint (|| true because it will fail to mount)
-# TODO: Figure out a better way to recover from the mount failure. Catching any
-# error is not a good idea.
-for path in /home /var /var/log /var/log/journal /etc /data /data /docker; do
-  zfs create -o mountpoint=$path zroot/ROOT$path || true
-done
-
-# Unmount everything for now
-zfs unmount -a
-
-# Set up posix ACL for the journal
-zfs set acltype=posixacl zroot/ROOT/var/log/journal
-zfs set xattr=sa zroot/ROOT/var/log/journal
-
-zpool set bootfs=zroot/ROOT zroot
-zpool export zroot
-
-# Import the pool by id, to ensure we get consistent mounting
-zpool import -l -d /dev/disk/by-id -R /mnt zroot
-# Set the cachefile for the pool, and then copy it over to the chroot fs
-zpool set cachefile=/etc/zfs/zpool.cache zroot
-mkdir -p /mnt/etc/zfs
-cp /etc/zfs/zpool.cache /mnt/etc/zfs/zpool.cache
+# Set up root fs filesystem and mount it
+format_root "${part_root}"
 
 # Make a /boot directory and mount our EFI boot partition there
 mkdir /mnt/boot
@@ -160,27 +129,22 @@ pacstrap /mnt \
   wget \
   whois
 
-# Generate an fstab and put it in the chroot environment. We only want the boot
-# partition in it; the rest gets handled by ZFS
-genfstab -t PARTUUID /mnt | grep -A 1 "$part_boot" > /mnt/etc/fstab
+make_fstab "${part_boot}" "${part_root}"
 
 # Set up the hostname and /etc/hosts
-function set_hostname() {
-  hostname=$(dialog --stdout --inputbox "Enter hostname" 0 0) || exit 1
-  clear
-  : ${hostname:?"hostname cannot be empty"}
+hostname=$(dialog --stdout --inputbox "Enter hostname" 0 0) || exit 1
+clear
+: ${hostname:?"hostname cannot be empty"}
 
-  echo "$hostname" > /mnt/etc/hostname
+echo "$hostname" > /mnt/etc/hostname
 
-  hostname_short="$(echo "$hostname" | cut -d '.' -f 1)"
-  cat <<EOF > /mnt/etc/hosts
+hostname_short="$(echo "$hostname" | cut -d '.' -f 1)"
+cat <<EOF > /mnt/etc/hosts
 127.0.0.1 localhost
 ::1       localhost
 ::1       $hostname $hostname_short
 127.0.1.1	$hostname $hostname_short
 EOF
-}
-set_hostname
 
 # Set the timezone
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
@@ -192,39 +156,38 @@ arch-chroot /mnt hwclock --systohc
 # duplicate contents on subsequent runs of the script.
 truncate --size 0 /mnt/etc/vconsole.conf
 
-# Set up tho locale. Setting this before installing the zfs modules means that
+# Set up the locale. Setting this before installing the zfs modules means that
 # the language setting should get picked up and be set correctly in the ramdisk.
 echo "en_US.UTF-8 UTF-8" > /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
-echo "KEYMAP=dvorak" >> /mnt/etc/vconsole.conf
+
+# If a keymap was passed, set it on the system
+if [ -n "${KEYMAP}" ]; then
+  echo "KEYMAP=${KEYMAP}" >> /mnt/etc/vconsole.conf
+fi
 
 # Set up the hooks correctly for allowing us to unlock the encrypted partitions.
 # Doing this here _should_ mean that when we install the zfs- modules below, the
 # kernels' ramdisks etc. should pick it up.
 sed -i \
-  's/^HOOKS.*/HOOKS=(base udev keyboard keymap autodetect modconf block encrypt zfs filesystems)/' \
+  "s/^HOOKS.*/HOOKS=($(hooks))/" \
   /mnt/etc/mkinitcpio.conf
 
-# Install zfs modules
-cat <<EOF >>/mnt/etc/pacman.conf
-[archzfs]
-Server = https://archzfs.com/\$repo/x86_64
-EOF
-
-arch-chroot /mnt pacman-key --recv-keys F75D9D76 --keyserver keyserver.ubuntu.com
-arch-chroot /mnt pacman-key --lsign-key F75D9D76
-arch-chroot /mnt pacman -Sy --needed --noconfirm zfs-linux zfs-linux-lts
+# Install any additional packages we want before rebooting.
+add_packages
 
 # Install the bootloader
 arch-chroot /mnt bootctl install
 
-# Enable SSH and nm
+# Enable SSH, nm and timesyncd
 arch-chroot /mnt systemctl enable sshd
 arch-chroot /mnt systemctl enable NetworkManager.service
 arch-chroot /mnt systemctl enable systemd-timesyncd.service
 
 # Set up wifi
+# TODO: Figure out why this seems not to work. I think there's a system uuid
+# that's somehow part of the nm config filename?
 if dialog --yesno "Configure wifi for target system?" 0 0; then
   wifi_ssid=$(dialog --stdout --inputbox "Enter wifi SSID" 0 0) || exit 1
   clear
@@ -259,11 +222,6 @@ method=auto
 EOF
 fi
 
-# Set default boot options, which are pretty straightforward. This will attempt
-# to mount the root fs from the zroot pool, and will prompt for encryption
-# passwords as though you'd invoked 'zpool import -l zroot'.
-boot_options="zfs=zroot rw"
-
 # Figure out what system we're installing on, in case we need to make any
 # customization to the boot options.
 product_name=$(dmidecode --string system-product-name)
@@ -277,7 +235,7 @@ if [ "${product_name}" == "XPS 15 9560" ]; then
 
   # We also need to set the following options to avoid hanging at some point
   # after boot, in improve power consumption
-  boot_options="${boot_options} nouveau.modeset=0 acpi_rev_override=1 enable_fbc=1 enable_psr=1 disable_power_well=0 pci=noaer"
+  boot_options="$(additional_boot_options) nouveau.modeset=0 acpi_rev_override=1 enable_fbc=1 enable_psr=1 disable_power_well=0 pci=noaer"
 fi
 
 # Write bootloader entries for the standard kernel also the LTS kernel, which
@@ -304,66 +262,24 @@ default arch
 timeout 3
 EOF
 
-function add_user() {
-  # figure out which user we want to use
-  user=$(dialog --stdout --inputbox "Enter admin username" 0 0) || exit 1
-  clear
-  : ${user:?"user cannot be empty"}
+# figure out which user we want to use
+user=$(dialog --stdout --inputbox "Enter admin username" 0 0) || exit 1
+clear
+: ${user:?"user cannot be empty"}
 
-  # Add the user
-  arch-chroot /mnt useradd -mU \
-    --uid 1185 \
-    -G wheel,uucp,video,audio,storage,games,input \
-    "$user"
+# Add the user
+arch-chroot /mnt useradd -mU \
+  --uid 1185 \
+  -G wheel,uucp,video,audio,storage,games,input \
+  "$user"
 
-  # Set the password
-  echo -n "$user password: "; arch-chroot /mnt passwd "$user"
-}
-add_user
+# Set the password
+echo -n "$user password: "; arch-chroot /mnt passwd "$user"
 
 # Write a first-boot file that we'll run the first time the system is
 # rebooted after running this install script. This will configure things
 # correctly so that the system continues booting.
-cat <<EOF > "/mnt/home/$user/first-boot.sh"
-#!/bin/bash
-
-set -euo pipefail
-
-echo "
-
-
-!! WE WILL NOW PERFORM FIRST-BOOT CONFIGURATION OF THE ZFS SYSTEM !!
-   DO NOT SKIP THIS OR YOUR SYSTEM WILL FAIL TO BOOT AGAIN
-
-   If you don't have an internet connection, sort that out and then
-   run './first-boot.sh'. Afterwards, remove the final line from
-   .bashrc so this doesn't run again.
-
-
-"
-
-# Check for internet
-ping -c 1 github.com > /dev/null
-
-sudo zpool set cachefile=/etc/zfs/zpool.cache zroot
-sudo systemctl enable zfs.target
-sudo systemctl enable zfs-import-cache
-sudo systemctl enable zfs-mount
-sudo systemctl enable zfs-import.target
-sudo zgenhostid \$(hostid)
-sudo mkinitcpio -p linux
-sudo mkinitcpio -p linux-lts
-
-echo "
-
-
-First run configuration has been completed. It's suggested that you restart
-before proceeding to do anything else. You can find the script that was used to
-install the system in \$(pwd).
-
-"
-
-EOF
+first_boot_script > "/mnt/home/$user/first-boot.sh"
 chmod +x "/mnt/home/$user/first-boot.sh"
 
 cat <<EOF >> "/mnt/home/$user/.bashrc"
@@ -379,16 +295,14 @@ arch-chroot /mnt chown -R "$user:" "/home/$user"
 echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
 
 # Set a random password for the root user.
-password="$(openssl rand -base64 32)"
+password="$(openssl rand -base64 64)"
 echo "root:$password" | chpasswd --root /mnt
 
 # Copy this script into the new installation for reference
 cp "$0" /mnt/home/$user/$(basename "$0")
 
 # Unmount everything to get ready for reboot
-umount /mnt/boot
-zfs umount -a
-zpool export zroot
+umount_all
 
 cat <<EOF
 
---
+++ zfs.sh
@@ -0,0 +1,136 @@
+#!/bin/sh
+
+set -eux
+
+# This function unmounts all filesystems, preparatory for rebooting.
+function umount_all() {
+  umount /mnt/boot
+  zfs umount -a
+  zpool export zroot
+}
+
+function clean() {
+  umount_all || true
+  zpool destroy zroot || true
+}
+
+function checkdeps() {
+  # Ensure we have the zfs module
+  modprobe zfs
+}
+
+# This should format and mount the root partition(s)
+function format_root() {
+  # Create a pool made up of the drive
+  zpool create -f zroot -m none $1
+
+  # Create the root (/), with default attributes (|| true because it will fail to
+  # mount)
+  # TODO: Figure out a better way to recover from the mount failure. Catching any
+  # error is not a good idea.
+  zfs create \
+    -o atime=off \
+    -o compression=on \
+    -o mountpoint=/ \
+    -o encryption=on \
+    -o keyformat=passphrase \
+    zroot/ROOT || true
+
+  # Create datasets for all the other partitions we want to isolate, setting
+  # their mountpoint (|| true because it will fail to mount)
+  # TODO: Figure out a better way to recover from the mount failure. Catching any
+  # error is not a good idea.
+  for path in /home /var /var/log /var/log/journal /etc /data /data /docker; do
+    zfs create -o mountpoint=$path zroot/ROOT$path || true
+  done
+
+  # Unmount everything for now
+  zfs unmount -a
+
+  # Set up posix ACL for the journal
+  zfs set acltype=posixacl zroot/ROOT/var/log/journal
+  zfs set xattr=sa zroot/ROOT/var/log/journal
+
+  zpool set bootfs=zroot/ROOT zroot
+  zpool export zroot
+
+  # Import the pool by id, to ensure we get consistent mounting
+  zpool import -l -d /dev/disk/by-id -R /mnt zroot
+  # Set the cachefile for the pool, and then copy it over to the chroot fs
+  zpool set cachefile=/etc/zfs/zpool.cache zroot
+  mkdir -p /mnt/etc/zfs
+  cp /etc/zfs/zpool.cache /mnt/etc/zfs/zpool.cache
+}
+
+function make_fstab() {
+  # Generate an fstab and put it in the chroot environment. We only want the boot
+  # partition in it; the rest gets handled by ZFS
+  genfstab -t PARTUUID /mnt | grep -A 1 $1 > /mnt/etc/fstab
+}
+
+function hooks() {
+  echo base udev keyboard keymap autodetect modconf block encrypt zfs filesystems
+}
+
+# Add packages before installing the bootloader and restarting.
+function add_packages() {
+  # Install zfs modules
+  cat <<EOF >>/mnt/etc/pacman.conf
+[archzfs]
+Server = https://archzfs.com/\$repo/x86_64
+EOF
+
+  arch-chroot /mnt pacman-key --recv-keys F75D9D76 --keyserver keyserver.ubuntu.com
+  arch-chroot /mnt pacman-key --lsign-key F75D9D76
+  arch-chroot /mnt pacman -Sy --needed --noconfirm zfs-linux zfs-linux-lts
+}
+
+# Set default boot options, which are pretty straightforward. This will attempt
+# to mount the root fs from the zroot pool, and will prompt for encryption
+# passwords as though you'd invoked 'zpool import -l zroot'.
+function additional_boot_options() {
+  echo zfs=zroot rw
+}
+
+function first_boot_script() {
+  cat <<EOF
+#!/bin/bash
+
+set -euo pipefail
+
+echo "
+
+
+!! WE WILL NOW PERFORM FIRST-BOOT CONFIGURATION OF THE ZFS SYSTEM !!
+   DO NOT SKIP THIS OR YOUR SYSTEM WILL FAIL TO BOOT AGAIN
+
+   If you don't have an internet connection, sort that out and then
+   run './first-boot.sh'. Afterwards, remove the final line from
+   .bashrc so this doesn't run again.
+
+
+"
+
+# Check for internet
+ping -c 1 github.com > /dev/null
+
+sudo zpool set cachefile=/etc/zfs/zpool.cache zroot
+sudo systemctl enable zfs.target
+sudo systemctl enable zfs-import-cache
+sudo systemctl enable zfs-mount
+sudo systemctl enable zfs-import.target
+sudo zgenhostid \$(hostid)
+sudo mkinitcpio -p linux
+sudo mkinitcpio -p linux-lts
+
+echo "
+
+
+First run configuration has been completed. It's suggested that you restart
+before proceeding to do anything else. You can find the script that was used to
+install the system in \$(pwd).
+
+"
+
+EOF
+}
